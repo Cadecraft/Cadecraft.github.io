eae1aeff3dea326c6d4728ba0d778f93905c418b
Caden Lee <cadendotlee@gmail.com>
2022-12-23 17:13:38 -0800

    added ccpiano section to extensions

--- static/landform-eclipse/enemies.js
+++ static/landform-eclipse/enemies.js
@@ -22,3 +22,24 @@ class Crab extends Entity {
         // attack: pincers (melee)
     }
 }
+// Cow Beast (toadd)
+class CowBeast extends Entity {
+    resetValsEntity() {
+        // Entity defs
+        this.name = "Cow Beast";
+        this.descr = "A cow-like animal from the plains which faces windward at all times; it produces milk";
+        this.drops = [14];
+        this.hpmax = 100;
+        this.friendly = false;
+        this.textures = [
+            "images/entities/enemy_crab_idle"
+        ];
+        this.moveStyle = 2;
+        // Overrides (optional)
+        this.phys_accel = 0.030; // originally 0.034
+    }
+    // Attack override
+    attack() {
+        // attack: pincers (melee)
+    }
+}
