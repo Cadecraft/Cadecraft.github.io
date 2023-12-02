5ad458d9409c8aae59bdaa8f392e7b1082cd98b8
Caden Lee <cadendotlee@gmail.com>
2023-12-02 09:44:43 -0800

    feat: massive images improve

---
+++ static/script3.js
@@ -0,0 +1,34 @@
+// Cadecraft: script (v3)
+
+// Remove JS warning (if JS is not enabled this will still appear)
+document.getElementById('jswarning').style.display = 'none';
+
+// Navigation
+const pageDivIds = [
+    "home", "games", "music", "chrome-extensions", "misc-projects"
+];
+// Show a specific page
+function showPage(pageId) {
+    for (let i = 0; i < pageDivIds.length; i++) {
+        let buttonClassName = document.getElementById('button_' + pageDivIds[i]).className;
+        if (pageDivIds[i] == pageId) {
+            // Show this page
+            document.getElementById('pg_' + pageDivIds[i]).style.display = 'inline';
+            if (!buttonClassName.includes('blue')) {
+                document.getElementById('button_' + pageDivIds[i]).className += ' blue';
+            }
+        } else {
+            // Do not show this page
+            document.getElementById('pg_' + pageDivIds[i]).style.display = 'none';
+            if (buttonClassName.includes('blue')) {
+                document.getElementById('button_' + pageDivIds[i]).className = buttonClassName.replace("blue", "");
+            }
+        }
+    }
+}
+// Add event listeners
+for (let i = 0; i < pageDivIds.length; i++) {
+    document.getElementById('button_' + pageDivIds[i]).addEventListener('click', function() {
+        showPage(pageDivIds[i]);
+    });
+}
