95b3e916285fc8cc65020a3acc99cee0a0988457
Cadecraft <cadendotlee@gmail.com>
2024-05-08 19:55:37 -0700

    yz: feat: all simple complete gameplay

--- static/yz/script.js
+++ static/yz/script.js
@@ -63,8 +63,22 @@ let currentState = {
 	dice: [0, 0, 0, 0, 0, 0], // 1-6, or 0 for none
 	diceLocked: [false, false, false, false, false, false],
 	possibleMoves: {}, // Move ID: points that could be earned
-	winner: -1 // -1 (not finished), 1, or 2
+	winner: -1 // -1 (the game is not finished), 1, 2, or 3 (tie)
 };
+
+// Reset the state to start a whole new game
+function restartComplete() {
+	currentState = {
+		scores_p1: {},
+		scores_p2: {},
+		turnPlayerId: 1,
+		turnRollNumber: 1,
+		dice: [0, 0, 0, 0, 0, 0],
+		diceLocked: [false, false, false, false, false, false],
+		possibleMoves: {},
+		winner: -1
+	};
+}
 
 // Count the number of dice of a value that exist (not including the sixth one)
 function countDice(diceValue) {
@@ -107,17 +121,22 @@ function calculatePossibilities() {
 		// Full house
 		currentState.possibleMoves[8] = 25;
 	}
+	// Small straight
+	if (
+		(countDice(1) >= 1 && countDice(2) >= 1 && countDice(3) >= 1 && countDice(4) >= 1)
+		|| (countDice(2) >= 1 && countDice(3) >= 1 && countDice(4) >= 1 && countDice(5) >= 1)
+		|| (countDice(3) >= 1 && countDice(4) >= 1 && countDice(5) >= 1 && countDice(6) >= 1)
+	) {
+		currentState.possibleMoves[9] = 30;
+	}
+	// Large straight
 	if (
 		(countDice(1) >= 1 && countDice(2) >= 1 && countDice(3) >= 1 && countDice(4) >= 1 && countDice(5) >= 1)
 		|| (countDice(2) >= 1 && countDice(3) >= 1 && countDice(4) >= 1 && countDice(5) >= 1 && countDice(6) >= 1)
 	) {
-		currentState.possibleMoves[9] = 30;
-	}
-	if (
-		countDice(1) >= 1 && countDice(2) >= 1 && countDice(3) >= 1 && countDice(4) >= 1 && countDice(5) >= 1 && countDice(6) >= 1
-	) {
 		currentState.possibleMoves[10] = 40;
 	}
+	// Chance
 	currentState.possibleMoves[12] = sumDice();
 }
 
@@ -138,6 +157,50 @@ function resetForNextTurn() {
 	currentState.turnRollNumber = 1;
 }
 
+// Commit a move (from clicking a rendered move button for the thisi-th move type)
+function commitMove(thisi) {
+	// If game already ended, cannot change anything
+	if (currentState.winner != -1) return;
+	// Add points
+	if (currentState.turnPlayerId == 1) {
+		if (thisi in currentState.possibleMoves) currentState.scores_p1[thisi] = currentState.possibleMoves[thisi];
+		else currentState.scores_p1[thisi] = 0;
+	} else {
+		if (thisi in currentState.possibleMoves) currentState.scores_p2[thisi] = currentState.possibleMoves[thisi];
+		else currentState.scores_p2[thisi] = 0;
+	}
+	resetForNextTurn();
+	currentState.turnPlayerId = 3 - currentState.turnPlayerId;
+	// Endgame check: set winner if necessary
+	let isGameFinished = true;
+	let finalscore_p1 = 0;
+	let finalscore_p2 = 0;
+	for (let i = 0; i < MOVES.length; i++) {
+		if (!(i in currentState.scores_p1) || !(i in currentState.scores_p2)) {
+			isGameFinished = false;
+			break;
+		}
+		finalscore_p1 += currentState.scores_p1[i];
+		finalscore_p2 += currentState.scores_p2[i];
+	}
+	if (isGameFinished) {
+		// Set the winner
+		// TODO: display the difference in scores when rendering the end screen?
+		if (finalscore_p1 == finalscore_p2) {
+			// Tie
+			// TODO: handle tie: duel to the death of chance (whoever gets the first highest roll)?
+			currentState.winner = 3;
+		} else if (finalscore_p1 > finalscore_p2) {
+			currentState.winner = 1;
+		} else {
+			currentState.winner = 2;
+		}
+	}
+	// Render
+	render();
+}
+
+
 // Render the page based on the current state
 function render() {
 	// Main bottom button
@@ -146,15 +209,20 @@ function render() {
 		// TODO: other features/styles?
 		// TODO: screen rotation
 		mainbutton.className = "bigmain button_p" + currentState.winner;
-		mainbutton.innerText =
-			"P" + currentState.turnPlayerId + " Wins! - Restart";
+		if (currentState.winner == 3) {
+			// Tie
+			mainbutton.innerText = "Tie! - Restart";
+		} else {
+			mainbutton.innerText =
+				"P" + currentState.winner + " Wins! - Restart";
+		}
 	} else {
 		// Player turn/roll count
 		// TODO: screen rotation
 		mainbutton.className = "bigmain button_p" + currentState.turnPlayerId;
 		if (currentState.turnRollNumber > MAX_ROLLS) {
-			// TODO: disable button
-			// TODO: set text
+			// TODO: disable button (make gray?)
+			mainbutton.className = "bigmain button_disabled";
 			mainbutton.innerText =
 				"P" + currentState.turnPlayerId + " - Out of rolls";
 		} else {
@@ -187,11 +255,7 @@ function render() {
 				newbutton.innerText = currentState.possibleMoves[i];
 				let thisi = i;
 				newbutton.addEventListener("click", () => {
-					currentState.scores_p1[thisi] = currentState.possibleMoves[thisi];
-					resetForNextTurn();
-					currentState.turnPlayerId = 3 - currentState.turnPlayerId;
-					render();
-					// TODO: endgame, refactor this into a function?
+					commitMove(thisi);
 				});
 				document.getElementById("row_" + i + "_p1").appendChild(newbutton);
 			} else {
@@ -201,15 +265,11 @@ function render() {
 				newbutton.innerText = "0";
 				let thisi = i;
 				newbutton.addEventListener("click", () => {
-					currentState.scores_p1[thisi] = 0;
-					resetForNextTurn();
-					currentState.turnPlayerId = 3 - currentState.turnPlayerId;
-					render();
-					// TODO: endgame
+					commitMove(thisi);
 				});
 				document.getElementById("row_" + i + "_p1").appendChild(newbutton);
 			}
-			// TODO: display the other player's current scores
+			// Other player's scores
 			document.getElementById("row_" + i + "_p2").innerHTML = "";
 			if (i in currentState.scores_p2) {
 				// Text: this many points earned
@@ -220,7 +280,6 @@ function render() {
 			}
 		} else {
 			// Player 2
-			// TODO: copy from above, changing 1 to 2
 			document.getElementById("row_" + i + "_p2").innerHTML = "";
 			if (i in currentState.scores_p2) {
 				// Text: this many points earned
@@ -232,11 +291,7 @@ function render() {
 				newbutton.innerText = currentState.possibleMoves[i];
 				let thisi = i;
 				newbutton.addEventListener("click", () => {
-					currentState.scores_p2[thisi] = currentState.possibleMoves[thisi];
-					resetForNextTurn();
-					currentState.turnPlayerId = 3 - currentState.turnPlayerId;
-					render();
-					// TODO: endgame, refactor this into a function?
+					commitMove(thisi);
 				});
 				document.getElementById("row_" + i + "_p2").appendChild(newbutton);
 			} else {
@@ -246,15 +301,11 @@ function render() {
 				newbutton.innerText = "0";
 				let thisi = i;
 				newbutton.addEventListener("click", () => {
-					currentState.scores_p2[thisi] = 0;
-					resetForNextTurn();
-					currentState.turnPlayerId = 3 - currentState.turnPlayerId;
-					render();
-					// TODO: endgame
+					commitMove(thisi);
 				});
 				document.getElementById("row_" + i + "_p2").appendChild(newbutton);
 			}
-			// TODO: display the other player's current scores
+			// Other player's scores
 			document.getElementById("row_" + i + "_p1").innerHTML = "";
 			if (i in currentState.scores_p1) {
 				// Text: this many points earned
@@ -267,10 +318,9 @@ function render() {
 	}
 }
 
-// TODO: impl dice click (only allows if on roll 2 or 3)
+// Dice locking feature on click
 for (let i = 0; i < 5; i++) {
 	document.getElementById("dice_" + (i + 1)).addEventListener("click", () => {
-		// TODO: impl
 		if (currentState.winner == -1 && (currentState.turnRollNumber == 2 || currentState.turnRollNumber == 3)) {
 			currentState.diceLocked[i] = !currentState.diceLocked[i];
 		}
@@ -282,7 +332,7 @@ for (let i = 0; i < 5; i++) {
 mainbutton.addEventListener("click", () => {
 	if (currentState.winner != -1) {
 		// Winner already decided: restart the entire game
-		// TODO: impl restarting the game
+		restartComplete();
 	} else {
 		// Player turn/roll
 		if (currentState.turnRollNumber > MAX_ROLLS) {
